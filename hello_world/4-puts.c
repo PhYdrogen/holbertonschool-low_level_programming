3a209aa7b3307c09481926f37db2f1b6fe4dbf0a
Gabriel <fossecruor@gmail.com>
2022-09-28 06:36:41 -0700

    betty doc lined

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/* ma fonction main va executer mon code
+ * puts prends en parametre des conts char*
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
