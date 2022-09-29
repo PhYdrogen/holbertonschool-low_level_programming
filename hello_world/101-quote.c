913bc39f22be6afc3262ae2e32b50fc367073f6b
Gabriel <fossecruor@gmail.com>
2022-09-29 00:26:16 -0700

    add basic

---
+++ hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 1 error ; 0 no error
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
