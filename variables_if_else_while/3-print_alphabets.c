e577b482f3c1fff7f73fe28c7b1b984bedb7512d
Gabriel <fossecruor@gmail.com>
2022-10-03 03:32:39 -0700

    add file 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 no error
+ * in ascii a = 97 and z = 122
+ * A = 65 and Z = 90 , $ = 36
+ */
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha != 36)
+	{
+		if (alpha > 91 && alpha < 123)
+		{
+		putchar(alpha);
+		alpha++;
+		}
+		if (alpha == 123)
+		alpha = 65;
+		if (alpha >= 65 && alpha <= 90)
+		{
+		putchar(alpha);
+		alpha++;
+		}
+		if (alpha == 91)
+		{
+			putchar(36);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
