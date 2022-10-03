56d55cf68ad177db8de13358edaf3c9f999d5cf6
Gabriel <fossecruor@gmail.com>
2022-10-03 08:00:36 -0700

    add file 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ * Return: 0 no error
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+	for(;i <= 122;i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
