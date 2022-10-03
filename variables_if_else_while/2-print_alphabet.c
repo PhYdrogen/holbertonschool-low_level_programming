7a0946af27b2487659abb4cf24e60576b01efd0d
Gabriel <fossecruor@gmail.com>
2022-10-03 03:02:50 -0700

    add file 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 no error
+ * in ascii a = 97 and z = 122
+ */
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha < 123)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	return (0);
+}
