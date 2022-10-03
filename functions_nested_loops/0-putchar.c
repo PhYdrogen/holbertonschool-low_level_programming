9de7bf132541b1ab2bd736af58a2e62da0fe6708
Gabriel <fossecruor@gmail.com>
2022-10-03 06:32:52 -0700

    add file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
  * main - entry point
  * Return: 0 no error
  */
 int main(void)
 {
-prinft("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
