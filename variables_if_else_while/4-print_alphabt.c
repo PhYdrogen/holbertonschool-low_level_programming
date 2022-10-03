aa580de4bab2d73ca121e849cce486eb5a2db061
Gabriel <fossecruor@gmail.com>
2022-10-03 04:29:18 -0700

    add file 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 no error
+ *
+ */
+int main(void)
+{
+	int alpha = 97;
+
+	while (alpha < 123)
+	{
+		if (101 == alpha || 113 == alpha)
+		{
+			alpha++;
+			continue;
+		}
+		else
+		{
+			putchar(alpha);
+			alpha++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
