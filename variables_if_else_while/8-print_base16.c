7d25746472276916bea344be6d3d3f7412cdadfe
Gabriel <fossecruor@gmail.com>
2022-10-03 04:49:28 -0700

    add file 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 no error
+ * in ascii 0 = 48, 9 = 57 [a-f] = 97 - 102
+ */
+int main(void)
+{
+	int nombre = 48;
+
+	while (nombre >= 48 && nombre <= 57 || nombre >= 97 && nombre <= 102)
+	{
+		putchar(nombre);
+		nombre++;
+		 if (nombre == 58)
+			nombre = 97;
+	}
+	putchar('\n');
+	return (0);
+}
