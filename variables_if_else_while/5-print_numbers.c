095f27785552dca93a79918899b1978394b50d6f
Gabriel <fossecruor@gmail.com>
2022-10-03 04:34:06 -0700

    add file 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 no error
+ *
+ */
+int main(void)
+{
+	int nombre = 0;
+
+	while(nombre <=9)
+	{
+	printf("%d", nombre);
+	nombre++;
+	}
+	putchar('\n');
+	return (0);
+}
