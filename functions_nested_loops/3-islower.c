de407073713a125b5500d7dd46bc104a5e43c304
Gabriel <fossecruor@gmail.com>
2022-10-04 00:21:13 -0700

    update main & add file 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - check if the char is lower
+ * Return: 1 if the char is lower, 0 if not
+ */
+int _islower(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 48 && c <= 57))
+	{
+		return (1);
+	}else
+		return (0);
+}
