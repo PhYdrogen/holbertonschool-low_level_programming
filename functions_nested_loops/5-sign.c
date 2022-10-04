a1788d23714ab4af3c30427013aca03e7974e0b3
Gabriel <fossecruor@gmail.com>
2022-10-04 05:22:47 -0700

    rename lol

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sing - it tells if the number is either positive or negative
+ * @n: the number to test
+ * Return: 1 if positive, 0 if equal to 0, -1 if negative
+ */
+int print_sing(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	} else
+	{
+		_putchar('0');
+		return (0);
+	}
+
+}
