c3cec779054994520168e5d83de971989b595865
Gabriel <fossecruor@gmail.com>
2022-10-04 10:28:59 -0700

    fixing converting char

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_last_digit - print le last digit of the number
  * @val: the number to test
@@ -7,23 +8,26 @@
 int print_last_digit(int val)
 {
 	int x;
-	int y;
-
-	x = 48 + (val % 10);
-	y = 48 + (val % -10);
+	char j;
 
 	if (val > 0)
 	{
-		_putchar(x);
+		x = (val % 10);
+		j = x + '0';
+		_putchar(j);
 		return (x);
-	} else if (val == 0)
+	}
+	else if (val < 0)
+	{
+		x = ((val % -10) * -1);
+		j = x + '0';
+		_putchar(j);
+		return (x);
+	}
+	else
 	{
 		_putchar('0');
 		return (0);
 	}
-	else
-	{
-		_putchar(y);
-		return (y);
-	}
+
 }
