81d2bfb07833cd5b4d67e8942285a0f301b706bd
Gabriel <fossecruor@gmail.com>
2022-10-04 04:48:21 -0700

    update main && add file 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check if the char is alphabetic
+ * Return: 1 if the char is lower, 0 if not
+ * @c: the letter convert in int
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122))
+	{
+		return (1);
+	} else
+		return (0);
+}
