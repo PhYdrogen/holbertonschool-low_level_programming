2d3a9c6b9d2d3717f3efd67ff658deeae2beb7b9
Gabriel <fossecruor@gmail.com>
2022-10-04 06:32:01 -0700

    fix betty typo

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 /**
  * _abs - it tells if the number is either positive or negative
  * @val: the number to test
- * Return: 
+ * Return: the value in positive
  */
 int _abs(int val)
 {
-	if ( val < 0 )
+	if (val < 0)
 		return (val * -1);
 	else
 		return (val);
