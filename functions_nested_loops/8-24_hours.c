733f08de25b2a1ce0f922e612f5dea891df97855
Gabriel <fossecruor@gmail.com>
2022-10-04 11:19:49 -0700

    add file 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * jack_bauer - print the timer from 00:00 to 23:59
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+
+	for (i = '0'; i <= '2'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '5' ; k++)
+			{
+				for (l = '0'; l <= '9' ; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(':');
+					putchar(k);
+					putchar(l);
+					putchar('\n');
+				}
+			}
+			if (i == '2' && j == '3')
+				break;
+		}
+	}
+}
