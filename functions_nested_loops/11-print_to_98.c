e6840d7277409ca00b07820985c0675873d7fb9c
Gabriel <fossecruor@gmail.com>
2022-10-05 05:13:50 -0700

    fix } and betty 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * print_to_98 - function that prints all natural numbers from n to 98
  * @n - number i have to reach for 98
  * Return: void
  */
-
 void print_to_98(int n)
 {
 	int x;
@@ -16,14 +14,15 @@ void print_to_98(int n)
 	{
 		while (n != 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n++;
 		}
 	} else
 	{
 		while (n != 98)
 		{
-			printf("%d, ",n);
+			printf("%d, ", n);
 			n--;
 	}
+}
 }
