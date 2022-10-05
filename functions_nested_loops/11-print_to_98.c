69c7ccc989987ab6247d4bdb68d45b6a0921c790
Gabriel <fossecruor@gmail.com>
2022-10-05 05:23:13 -0700

    quick fix @n 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
  * print_to_98 - function that prints all natural numbers from n to 98
- * @n - number i have to reach for 98
+ * @n: number i have to reach for 98
  * Return: void
  */
 void print_to_98(int n)
