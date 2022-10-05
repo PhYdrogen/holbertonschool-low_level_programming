d23b571f313d5fb83b89f228cfea00b14af3e1e0
Gabriel <fossecruor@gmail.com>
2022-10-05 01:41:09 -0700

    add file 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two int
+ * @ch1: number to add
+ * @ch2: number two to add
+ * Return: the adition of the two
+ */
+int add(int ch1, int ch2)
+{
+	return ch1+ch2;
+}
