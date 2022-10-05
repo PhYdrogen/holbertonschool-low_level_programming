f8096f00d9ca8f6d9e268ee2d78969b0ac041a97
Gabriel <fossecruor@gmail.com>
2022-10-05 01:16:27 -0700

    fix betty file 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,36 +5,36 @@
  */
 void times_table(void)
 {
-    int x;
-    int k;
-    int ch = 0;
-    int ch2 = ' ';
-    int ch3 = ' ';
-    int tmp;
+	int x;
+	int k;
+	int ch = 0;
+	int ch2 = ' ';
+	int ch3 = ' ';
+	int tmp;
 
-    for (x = 0 ; x <= 9 ; x++)
-    {  
-        for (k = 0 ; k <= 9 ; k++)
-        {
-            ch = k * x;
-            if ( ch >= 10)
-            {
-                tmp = ch;
-                ch = ch % 10;
-                ch2 = (tmp - ch) / 10;
-                ch2 = ch2 + '0';
-            }
-            ch = ch + '0';
-            putchar(ch3);
-            putchar(ch2);
-            putchar(ch);
-            putchar(',');
-            ch = ch - '0';
-            ch++;
-        }
-        putchar('\n');
-        ch = 0;
-        ch2 = ' ';
-    }
-    
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		for (k = 0 ; k <= 9 ; k++)
+		{
+			ch = k * x;
+			if (ch >= 10)
+			{
+				tmp = ch;
+				ch = ch % 10;
+				ch2 = (tmp - ch) / 10;
+				ch2 = ch2 + '0';
+			}
+			ch = ch + '0';
+			_putchar(ch3);
+			_putchar(ch2);
+			_putchar(ch);
+			_putchar(',');
+			ch = ch - '0';
+			ch++;
+		}
+		_putchar('\n');
+		ch = 0;
+		ch2 = ' ';
+	}
+
 }
