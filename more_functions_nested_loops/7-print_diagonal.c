04f471db17baeee4b2a5b4ec54f92a7d39df0b8b
Gabriel <fossecruor@gmail.com>
2022-10-07 02:58:57 -0700

    add file 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - print \ in range to make a diagonal
+ * @n: the number of time the \ will be printed
+ * Return: none
+ */
+void print_diagonal(int n)
+{
+	int x;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x != n; x++)
+		{
+			for (j = 0; j <= x; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
