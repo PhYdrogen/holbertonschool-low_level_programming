b9062ae1c160611e1510bb5c5346f7c7a03a64ff
Gabriel <fossecruor@gmail.com>
2022-10-07 04:58:57 -0700

    fix space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x <= 100; x++)
+	printf("1 ");
+	for (x = 2; x <= 100; x++)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		else if ((x % 3) == 0)
-			printf("Fizz ");
+			printf(" Fizz");
 		else if ((x % 5) == 0)
-			printf("Buzz ");
+			printf(" Buzz");
 		else
-			printf("%d ", x);
+			printf(" %d", x);
 	}
 	printf("\n");
 	return (0);
