7e322b7b0567f5d3a32df80d59efa5b447867514
Gabriel <fossecruor@gmail.com>
2022-10-07 02:04:32 -0700

    add file and update main

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *	print_numbers - print number from 0 to 9
+ *	Return: no return
+ */
+void print_numbers(void)
+{
+	int x; 
+	char j = '0';
+
+	for(x = 0; x < 10; x++)
+	{
+		_putchar(j);
+		j++;
+	}
+	_putchar('\n');
+}
