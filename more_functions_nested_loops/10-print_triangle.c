22021cdb992b7b40166728492e0fbb11643e6602
Gabriel <fossecruor@gmail.com>
2022-10-07 05:58:44 -0700

    add file 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle for each size
+ * @size: number representing the size of the triangle
+ * Return: none
+ */
+void print_triangle(int size)
+{
+	int x;
+	int j;
+	int k = size;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (x = 1; x <= size; x++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j >= k)
+				_putchar(35);
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+		k--;
+	}
+	_putchar('\n');
+}
