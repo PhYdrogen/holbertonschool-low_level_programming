4808a5e0c419cf9f2571c9d2721d2e6db5e2133f
Gabriel <fossecruor@gmail.com>
2022-10-07 00:51:55 -0700

    add file 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * more_numbers - print 0 to 14 with _printchar
+ * Return: none
+ */
+void more_numbers(void)
+{
+	int x;
+	int j;
+	char k = '0';
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+			{
+			_putchar(k);
+			k++;
+			}
+			else
+			{
+				if (k > '9')
+					k = '0';
+				_putchar('1');
+				_putchar(k);
+				k++;
+			}
+		}
+		_putchar('\n');
+		k = '0';
+	}
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,3 +5,4 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_most_numbers(void);
+void more_numbers(void);
