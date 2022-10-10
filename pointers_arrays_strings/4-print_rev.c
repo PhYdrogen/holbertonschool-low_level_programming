0c383eb80eecf7ccd00b82714d260a9d76551918
Gabriel <fossecruor@gmail.com>
2022-10-10 07:33:43 -0700

    add file 4 and update main

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_rev - print the param s in reverse
+ * @s: list of char
+ * Return: none
+ */
+void print_rev(char *s)
+{
+	int x;
+	int i;
+
+	/*strlen*/
+	for (x = 0; *(s + x) != '\0'; x++)
+		{}
+	for (i = x; i > 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
