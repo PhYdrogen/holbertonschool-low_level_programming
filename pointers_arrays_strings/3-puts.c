e898b8839ed60fe82eeb23c6aaabb5beaad7bc54
Gabriel <fossecruor@gmail.com>
2022-10-10 06:19:50 -0700

    update main and add file 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string followed by a new line
+ * @str: the string
+ * Return: no return value
+ */
+void _puts(char *str)
+{
+	puts(str);
+	puts('\n');
+}
