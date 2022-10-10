ffea319dcaf18397268c1651192f6c79b07fdbad
Gabriel <fossecruor@gmail.com>
2022-10-10 11:57:35 -0700

    forget lib

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * puts2 - print every other line
  * @str: take as input string
@@ -6,7 +7,7 @@
  */
 void puts2(char *str)
 {
-	int x, y;
+	int x;
 
 	for (x = 0; str[x] != '\0'; x++)
 	{
