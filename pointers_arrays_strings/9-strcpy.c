bc3a14ccdba822294fa234bdde85e94c44d399e2
Gabriel <fossecruor@gmail.com>
2022-10-11 01:54:58 -0700

    add file 9 and update main

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - copy string to a destination
+ * @src: the source to copy
+ * @dest: the destination of the copied string
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x, j;
+
+	for (j = 0; src[j] != '\0'; j++)
+		{}
+	for (x = 0; x < j; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
