87369fb420c6220de197f07d71457a53c7a64f7b
Gabriel <fossecruor@gmail.com>
2022-10-11 06:02:26 -0700

    update main and add file 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string to a pointer
+ * @dest: to destination
+ * @src: from source
+ * @n: the number of char max to copy from source
+ * Return: as pointer the copy of src with dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+	for(x = 0; (x < n) && (src[x] != '\0'); x++)
+	{
+		dest[x] = src[x];
+	}
+	for (;x < n; x++)
+		dest[x] = '\0';
+	return (dest);
+
+}
