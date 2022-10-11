9c03e3b50a70591a04a587add7cb469a28f50979
Gabriel <fossecruor@gmail.com>
2022-10-11 00:19:38 -0700

    add file 7 and update main

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - print half of the string
+ * @str: the string going to be cut in half like a potato
+ * Return: none
+ */
+void puts_half(char *str)
+{
+	int x, y;
+
+	for (x = 0; str[x] != '\0'; x++)
+		{}
+	if ((x % 2) == 0)
+	{
+		y = x / 2;
+		for (; str[y] != str[x]; y++)
+			_putchar(str[y]);
+	}
+	else
+	{
+		y = (x - 1) / 2;
+		for (; str[y] != str[x]; y++)
+			_putchar(str[y]);
+	}
+	_putchar('\n');
+}
