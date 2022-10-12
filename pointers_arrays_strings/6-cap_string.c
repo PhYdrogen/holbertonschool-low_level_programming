eb1abe251e3babc3343861f01c8c9b9de05c3d2e
Gabriel <fossecruor@gmail.com>
2022-10-12 01:19:04 -0700

    add file 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * cap_string - take string as input and out is capitalized with some condition
+ * @s: the string to cap
+ * Return: Return the string as capitalized
+ */
+char *cap_string(char *s)
+{
+        int x, y;
+
+        for (x = 0; s[x] != '\0'; x++)
+        {}
+        for (y = 0; y < x; y++)
+        {
+                if (s[y] == (' ') || (s[y] == '\t') || (s[y] == '\n') || (s[y] == ',') || (s[y] ==  ';') || (s[y] == '.') || (s[y] == '!') || (s[y] == '?') || (s[y] == '"') || (s[y] == '(') || (s[y] == ')') || (s[y] == '{') || (s[y] == '}') )
+                {
+                  if (s[y+1] >= 97 && s[y+1] <= 122)
+                        s[y+1] = s[y+1] - 32;
+                }
+        }
+  return (s);
+}
