86a2a9b867647a4d06ba10dbad8b137caf7d3a9c
Gabriel <fossecruor@gmail.com>
2022-10-12 02:42:59 -0700

    update betty for file 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int x;
-	for(x = 0; (x < n) && (src[x] != '\0'); x++)
+
+	for (x = 0; (x < n) && (src[x] != '\0'); x++)
 	{
 		dest[x] = src[x];
 	}
-	for (;x < n; x++)
+	for (; x < n; x++)
 		dest[x] = '\0';
 	return (dest);
 
