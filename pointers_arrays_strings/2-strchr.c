23e19f766742de8355313841588291982372413c
Gabriel <fossecruor@gmail.com>
2022-10-12 09:38:05 -0700

    wrong file lolk

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,12 +7,21 @@
  */
 char *_strchr(char *s, char c);
 {
-	int x;
-
-	for (x = 0; s[x] != '\0'; x++)
-	{
-		if (s[x] == c)
-			return (s[x]);
-	}
-	return (NULL);
+  int x, y, z;
+  char lt[9];
+  char *ltp = lt;
+  
+  for (x = 0; s[x] != '\0'; x++)
+    {}
+  /* x is my string length */
+  for (y = 0; s[y] != c; y++)
+    {}
+  /* y is where i found my char*/
+  for (z = 0; z < x; y++)
+    {
+      lt[z] = s[y];
+      z++;
+    }
+  lt[z+1] = '\0';
+  return (ltp);
 }
