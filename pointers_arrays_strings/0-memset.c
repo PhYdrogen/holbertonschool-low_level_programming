09fc96ec0b59ef47d517e42335847bb8cb9d9f37
Gabriel <fossecruor@gmail.com>
2022-10-12 08:05:04 -0700

    add file 0 and update main

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fill memory with constant btye
+ * @s: memory area
+ * @b: constant byte
+ * @n: n time
+ * Return: Returns a pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		s[x] = b;
+	}
+	return (s);
+}
