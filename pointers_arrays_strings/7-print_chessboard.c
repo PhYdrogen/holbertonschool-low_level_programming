85ef1f6f03343ace9ffebb1b11b3081f0386dc16
Gabriel <fossecruor@gmail.com>
2022-10-14 01:09:58 -0700

    add file 7 and update main

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - function that print the chessboard
+ * @a:
+ * Return: none
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	for (i=0;i<8;i++)
+	{
+		for (j=0;j<8;j++)
+			printf("%c",a[i][j]);
+		printf("\n");
+	}
+}
