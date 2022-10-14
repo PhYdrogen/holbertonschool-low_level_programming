fd1ccc1a92e993a375382586588d9c12a2d02b28
Gabriel <fossecruor@gmail.com>
2022-10-14 01:12:00 -0700

    betty doc and liner

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 /**
  * print_chessboard - function that print the chessboard
- * @a:
+ * @a: the array to chess
  * Return: none
  */
 void print_chessboard(char (*a)[8])
 {
-	int i,j;
+	int i, j;
 
-	for (i=0;i<8;i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j=0;j<8;j++)
-			printf("%c",a[i][j]);
+		for (j = 0; j < 8; j++)
+			printf("%c", a[i][j]);
 		printf("\n");
 	}
 }
