68d5654d1b6412e272e0c62f37cc313ef39d326f
Gabriel <fossecruor@gmail.com>
2022-10-15 10:37:18 -0700

    re do

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stddef.h>
 #include "main.h"
-
+/**
+ * _strchr - search for sign in sky
+ * @s: string of char
+ * @c: letter to find
+ * Return: pointer of string
+ */
 char *_strchr(char *s, char c)
 {
   while (*s++ != '\0')
