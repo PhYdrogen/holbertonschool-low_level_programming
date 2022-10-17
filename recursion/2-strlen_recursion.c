230b207faac96de3929755f329cfea766e39f8cd
Gabriel <fossecruor@gmail.com>
2022-10-17 04:43:14 -0700

    add file for testing

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - output the length of string
+ * @s: string
+ * Return: in int the number of char s has
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return
+	_strlen_recursion();
+}
