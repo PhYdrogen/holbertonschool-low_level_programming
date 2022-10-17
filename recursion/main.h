3e80470e1af9845cb514b083def3428e9b9b5a36
Gabriel <fossecruor@gmail.com>
2022-10-17 03:25:55 -0700

    forgot putchar

--- recursion/main.h
+++ recursion/main.h
@@ -1,4 +1,4 @@
-
+#include <unistd.h>
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -6,3 +6,4 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int _putchar(char c);
