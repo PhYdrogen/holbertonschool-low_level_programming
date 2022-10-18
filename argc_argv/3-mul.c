a53a2967906365df912c9c3ac5e7fa5fb6d88b47
Gabriel <fossecruor@gmail.com>
2022-10-18 06:53:07 -0700

    add file 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: number of arg
+ * @argv: list of arg
+ * Return: 0 if no error
+ */
+int main(int argc, char *argv[])
+{
+
+	if (argc == 3)
+	{
+		int res = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d", res);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
