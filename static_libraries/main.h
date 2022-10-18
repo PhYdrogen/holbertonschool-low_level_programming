c65f2ac93baa4ec1f50fe89666a23758eefe6ba6
Gabriel <fossecruor@gmail.com>
2022-10-18 05:23:05 -0700

    betty fix 3!

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 #include <unistd.h>
@@ -24,4 +24,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
