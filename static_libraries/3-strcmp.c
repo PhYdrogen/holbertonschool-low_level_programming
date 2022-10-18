e8b051e227d8851ccc265d91709694ed3834c20a
Gabriel <fossecruor@gmail.com>
2022-10-18 04:53:08 -0700

    fix mistake i made long time ago

---
+++ static_libraries/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compare two string and tell if it's the same
+ * @s1: first string
+ * @s2: second string
+ * Return: in int the first letter of diff
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+	}
+	return (0);
+}
