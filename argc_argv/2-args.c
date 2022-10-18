baf5abc6818a24102d1bdfa1bad54c369dd7d8af
Gabriel <fossecruor@gmail.com>
2022-10-18 06:33:23 -0700

    betty coding style 😎

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -9,10 +9,11 @@ int main(int argc, char *argv[])
 {
 	(void)argc;
 
+	printf("%s\n", argv[0]);
 	while (*argv++)
 	{
 		if (*argv)
-			printf("%s\n",*argv);
+			printf("%s\n", *argv);
 
 		else
 			return (0);
