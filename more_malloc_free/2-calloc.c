d4992d47d1fe95485b513d90feb0ac4199d439ab
Gabriel <fossecruor@gmail.com>
2022-10-25 08:04:44 -0700

    raise error if 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	int x;
 
-	if ((nmemb || size) == 0)
+	if ((nmemb == 0 || size == 0))
 		return (NULL);
 
 	p = malloc(size * nmemb);
