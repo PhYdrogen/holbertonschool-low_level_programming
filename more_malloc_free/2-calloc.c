e51b848c3c6f832c99cff78e163c3229e3e11b0f
Gabriel <fossecruor@gmail.com>
2022-10-25 07:47:21 -0700

    add content of file2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _calloc - it's like calloc
+ * @nmemb: number of box in the memory
+ * @size: the size of the calloc
+ * Return: a pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	int x;
+
+	if ((nmemb || size) == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+
+	if (p == NULL)
+		return (NULL);
+	for (x = 0; x < (int) nmemb; x++)
+	{
+		p[x] = 0;
+	}
+	return (p);
+}
