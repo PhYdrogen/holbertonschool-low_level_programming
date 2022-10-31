6ba50ae3ed127721c58d40ea29cb28a53acf8b58
Gabriel <fossecruor@gmail.com>
2022-10-31 05:50:10 -0700

    update proto, free_dog not free

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,5 +18,5 @@ typedef	struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free(dog_t *d);
+void free_dog(dog_t *d);
 #endif
