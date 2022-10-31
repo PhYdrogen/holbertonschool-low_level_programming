d368aeea1ac05cd7bb8761293998bf773a7c7184
Gabriel <fossecruor@gmail.com>
2022-10-31 02:57:14 -0700

    adding new type for dog struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: his age in dog year
  * @owner: the master of the dog
  */
-struct dog
+typedef	struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
