94505e4212ef50799cee640ddd85ca231aa96c5b
Gabriel <fossecruor@gmail.com>
2022-10-31 02:20:04 -0700

    ISO 90

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,10 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
 	struct dog;
 
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
