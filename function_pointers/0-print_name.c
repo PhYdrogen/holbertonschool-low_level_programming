eac2a413e4e1b62553796e7949bae66b73b81a5c
Gabriel <fossecruor@gmail.com>
2022-11-01 06:06:21 -0700

    fixing null char

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,6 +6,8 @@
  */
 void print_name(char *name, void(*f)(char *))
 {
+	if (f == NULL)
+		return;
 	(*f)(name);
 }
 
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FNC_PTR
 #define FNC_PTR
+#include <stdlib.h>
+#include <stdio.h>
 void print_name(char *name, void(*f)(char *));
 #endif
