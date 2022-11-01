b1b991f874564c31d97d5c3c3df0491cacc36314
Gabriel <fossecruor@gmail.com>
2022-11-01 07:48:56 -0700

    add file

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - print each number of an array
+ * @array: the array
+ * @size: the size
+ * @action: the fuction it calls
+ * Return: none
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int x;
+
+	if ((array == NULL) | (action == NULL))
+		return;
+	for (x = 0; x <= (int)size; x++)
+	{
+		action(array[x]);
+	}
+}
