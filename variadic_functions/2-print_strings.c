6857b3c705277b33270b653db579b3e5a2154f03
Gabriel <fossecruor@gmail.com>
2022-11-02 07:57:27 -0700

    add code like file 1 but changed

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+/**
+ * print_strings- print all strings in format
+ * @separator: the separator of cheat num
+ * @n: the number of elem
+ *Return: none
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	char *k;
+	va_list liste;
+
+	va_start(liste, n);
+	i = n;
+	if (separator == NULL)
+		separator = "";
+
+	for (; i > 0; i--)
+	{
+		k = va_arg(liste, char *);
+		if (i == 1)
+			printf("%s", k);
+		else
+			printf("%s%s", k, separator);
+	}
+	va_end(liste);
+	printf("\n");
+}
