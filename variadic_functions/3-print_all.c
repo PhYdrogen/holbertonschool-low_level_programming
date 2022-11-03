d53dd694eefc737ea8d171d5476b9b243674d808
Gabriel <fossecruor@gmail.com>
2022-11-03 04:28:13 -0700

    finish line

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,37 +11,39 @@ void print_all(const char * const format, ...)
 	va_list liste;
 	char *s, *sep;
 
+	if (format == NULL)
+	{ printf("\n");
+		return; }
 	va_start(liste, format);
 	n = strlen(format);
 	type = format;
 	sep = ", ";
 	while (n > 0)
 	{
-		if (n == 1)
-			sep = "";
+		while (n == 1)
+		{ sep = "";
+			break; }
 		switch (*type)
 		{
 		case 'c':
 			printf("%c%s", va_arg(liste, int), sep);
-			n--;
 			break;
 		case 'i':
 			printf("%d%s", va_arg(liste, int), sep);
-			n--;
 			break;
 		case 'f':
 			printf("%f%s", va_arg(liste, double), sep);
-			n--;
 			break;
 		case 's':
 			s = va_arg(liste, char*);
 			if (s == NULL)
 				s = "(nil)";
 			printf("%s%s", s, sep);
-			n--;
 			break;
 		default:
-			n--;
-		} type++;
+			break;
+		}
+		n--;
+		type++;
 	} printf("\n");
 	va_end(liste); }
