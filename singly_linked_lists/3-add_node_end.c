6817af10c900de76d28078f56805fac5b6f739c5
Gabriel <fossecruor@gmail.com>
2022-11-14 09:06:46 -0800

    add if else and whiteboard it

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,24 +11,28 @@ list_t *add_node_end(list_t **head, const char *str)
 	struct list_s *node, *tmp;
 
 	node = malloc(sizeof(list_t));
-	tmp = *head;
 
 	if (!node)
 		exit(99);
-
-	if (!head)
-	{
-		while ((*head)->next != NULL)
-		{
-			*head = (*head)->next;
-		}
-	}
 
 	node->str = strdup(str);
 	node->len = strlen(str);
 	node->next = NULL;
 
-	(*head)->next = node;
-	*head = tmp;
+
+	if (*head != NULL)
+	{
+		tmp = *head;
+		while ((*head)->next != NULL)
+		{
+			*head = (*head)->next;
+		}
+		(*head)->next = node;
+		*head = tmp;
+	}
+	else
+	{
+		(*head) = node;
+	}
 	return (node);
 }
