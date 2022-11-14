84cf0be040dcb8ec9b2b1016c8113b68a86445d8
Gabriel <fossecruor@gmail.com>
2022-11-14 01:46:16 -0800

    added check for NULL

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -9,14 +9,18 @@ size_t list_len(const list_t *h)
 	int count = 0;
 	struct list_s *temp;
 
-	temp = malloc(sizeof(list_t));
-	if (h->next == 0)
-		count++;
-	while (h != NULL && (temp->next != h->next))
+	if (h)
 	{
-		temp->next = h->next;
-		h = h->next;
-		count++;
+
+		temp = malloc(sizeof(list_t));
+		if (h->next == 0)
+			count++;
+		while (h != NULL && (temp->next != h->next))
+		{
+			temp->next = h->next;
+			h = h->next;
+			count++;
+		}
 	}
 	return (count);
 }
