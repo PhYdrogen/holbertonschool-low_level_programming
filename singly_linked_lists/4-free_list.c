9dde277267266fed4700c097ef9c44f8c8281515
Gabriel <fossecruor@gmail.com>
2022-11-14 09:29:45 -0800

    add prototype for file 4 and add free with while loop)

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_list - free my liked list
+ * @head: the header
+ * Return: none
+ */
+void free_list(list_t *head)
+{
+	struct list_s *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+	free(tmp);
+
+}
