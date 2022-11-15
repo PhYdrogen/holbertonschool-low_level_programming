25c8bfdabc98b188f98f0c6d534c414d3db99f00
Gabriel <fossecruor@gmail.com>
2022-11-15 09:26:45 -0800

    adding if NULL header

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp2;
-	int n;
+	int n = 0;
 
-	n = (*head)->n;
-	tmp2 = (*head)->next;
-	free(*head);
-	(*head) = tmp2;
+	if (*head != NULL)
+	{
+		n = (*head)->n;
+		tmp2 = (*head)->next;
+		free(*head);
+		(*head) = tmp2;
+	}
 	return (n);
 }
