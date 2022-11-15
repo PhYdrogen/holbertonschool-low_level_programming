c3b921ff97b1c24dbae90ddf2a78aac865bd929f
Gabriel <fossecruor@gmail.com>
2022-11-15 09:25:08 -0800

    add content for file 6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * pop_listint - pop the last elem
+ * @head: the header
+ * Return: the head node data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp2;
+	int n;
+
+	n = (*head)->n;
+	tmp2 = (*head)->next;
+	free(*head);
+	(*head) = tmp2;
+	return (n);
+}
