c08b762dab4d83cc95a5f2514c5dd793bbddc894
Gabriel <fossecruor@gmail.com>
2022-11-15 07:47:02 -0800

    forget the *

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,23 +1,21 @@
 #include "lists.h"
 
 /**
- * free_listint - free a list in order to clean memory
+ * free_listint2 - free a list in order to clean memory
  * @head: the mem
  * Return: none
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	struct listint_s *tmp;
 
 	if (head == NULL)
-		exit(99);
+		return;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		tmp = head->next;
-		free(head);
-		head = tmp;
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
 	}
-	free(tmp);
-	head = NULL;
 }
