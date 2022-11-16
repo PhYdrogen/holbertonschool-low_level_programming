549bde06dc99e35f6df10e01ea734ca35202f60a
Gabriel <fossecruor@gmail.com>
2022-11-16 00:55:39 -0800

    add content for file 9

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - func that insert new node at given position
+ * @head: the header
+ * @idx: the index to insert
+ * @n: the data
+ * Return: the node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *tmp, *tmp2;
+	unsigned int count;
+
+	count = 0;
+	/*init struct node*/
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (0);
+
+	/*make a save of the header*/
+	tmp2 = *head;
+	/*affect node value*/
+	new->n = n;
+
+	while ((*head) != NULL && count < idx)
+	{
+		if (count == (idx - 1))
+		{
+			tmp = (*head)->next;
+			(*head)->next = new;
+			new->next = tmp;
+			*head = tmp2;
+			return (new);
+		}
+		*head = (*head)->next;
+		count++;
+	}
+
+	return (NULL);
+}
