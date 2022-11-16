45aa1c96d41f4718f3e1f6025728768280c7dabf
Gabriel <fossecruor@gmail.com>
2022-11-16 00:29:38 -0800

    add content for file 8

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - make a sum of the linked list
+ * @head: the header
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
