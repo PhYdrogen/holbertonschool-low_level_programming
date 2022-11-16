6408c76873b3998cdedd2f675737b6602b4c19de
Gabriel <fossecruor@gmail.com>
2022-11-16 05:35:26 -0800

    add content for file 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - delete node
+ * @head: the header
+ * @index: the index to delete
+ * Return: the succes of the mission
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *tmp2;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+	/*saving head position*/
+	tmp = *head;
+
+	if (index == 0)
+	{
+		if ((*head)->next != NULL)
+		{
+			*head = (*head)->next;
+			free(tmp);
+			return (1);
+		}
+		else
+		{
+			free(*head);
+			*head = NULL;
+			return (1);
+		}
+	}
+	while ((*head)->next != NULL && count < index)
+	{
+		if (((count + 1) - index) == 0)
+		{
+			tmp2 = *head;
+		}
+		*head = (*head)->next;
+		count++;
+	}
+	/*delete node and connect to the next*/
+	tmp2->next = (*head)->next;
+	free(*head);
+	*head = tmp;
+	return (1);
+}
