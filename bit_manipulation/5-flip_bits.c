dddc6161f6a89f6922333b9cf28455ccdbbda11a
Gabriel <fossecruor@gmail.com>
2022-11-18 05:44:41 -0800

    adding content for file 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - how many bit to flip
+ * @n: the first numb
+ * @m: the second numb
+ * Return: the number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int x = 0, count = 0, nd = 0, md = 0;
+
+	for (; x < 65; x++)
+	{
+		nd = n & (1 << x);
+		md = m & (1 << x);
+		if (nd != md)
+		{
+			count++;
+		}
+	}
+	return (count);
+}
