773c9f738c1e7b27f6d069acbc27af826f514502
Gabriel <fossecruor@gmail.com>
2022-11-18 05:56:55 -0800

    modify arch if else

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -8,9 +8,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int x = 0, count = 0, nd = 0, md = 0;
+	int x = 0, count = 0, nd = 0, md = 0, arch = 0;
 
-	for (; x < 64; x++)
+	if (n == 4294967295)
+		arch = 64;
+	else
+		arch = 32;
+
+	for (; x < arch; x++)
 	{
 		nd = n & (1 << x);
 		md = m & (1 << x);
