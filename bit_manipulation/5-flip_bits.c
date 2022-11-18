69f8be91b0e05723314e3104362efc504de7c1aa
Gabriel <fossecruor@gmail.com>
2022-11-18 05:48:15 -0800

    change the loop for lower

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int x = 0, count = 0, nd = 0, md = 0;
 
-	for (; x < 65; x++)
+	for (; x < 32; x++)
 	{
 		nd = n & (1 << x);
 		md = m & (1 << x);
