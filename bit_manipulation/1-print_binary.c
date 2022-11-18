0f64e9067f3c02d0151f67a5a25a82a8269f6021
Gabriel <fossecruor@gmail.com>
2022-11-18 08:09:26 -0800

    i also found a way on the web

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -7,21 +7,10 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		_putchar('0');
-
-	while (n > 0)
 	{
-		if ((n & 1) == 1)
-		{
-			_putchar('1');
-			n = n >> 1;
-		}
-		else
-		{
-			_putchar('0');
-			n = n >> 1;
-		}
+		if (n > 1)
+			print_binary(n >> 1);
 
+		_putchar((n & 1) + '0');
 	}
 }
