7c4766ee726902dbd27b7c8a10cae14137e63f34
Gabriel <fossecruor@gmail.com>
2022-11-18 07:34:28 -0800

    change alot of things

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -8,17 +8,26 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int x = 0, count = 0, nd = 0, md = 0, arch = 0;
+	int x = 0, count = 0, arch = 0;
+	unsigned long int nd = 0, md = 0;
 
-	if (n > 4294967290)
+	if (n >= 4294967295)
 		arch = 64;
 	else
 		arch = 32;
 
 	for (; x < arch; x++)
 	{
-		nd = n & (1 << x);
-		md = m & (1 << x);
+		if (x == 32)
+		{
+			nd /= 2;
+			md /= 2;
+		}
+		else
+		{
+			nd = n & (1 << x);
+			md = m & (1 << x);
+		}
 		if (nd != md)
 		{
 			count++;
