7e0a3c29427f03cbf2a87a29f547ddb688bb61c4
Gabriel <fossecruor@gmail.com>
2022-11-21 11:06:49 -0800

    remove check for open and add if for read and write

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,7 +8,7 @@
  */
 int main(int ac, char **av)
 {
-	int fd[2], i = 0, nbchar = 1;
+	int fd[2], i = 0, nbchar = 1, size = 0;
 	char buffer[TAILLE_BUFF];
 
 	if (ac != 3)
@@ -18,21 +18,23 @@ int main(int ac, char **av)
 	}
 
 	fd[0] = open(av[1], O_RDONLY);
-	if (fd[0] == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	fd[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
-	if (fd[1] == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	while (nbchar > 0)
 	{
-		nbchar = write(fd[1], buffer, read(fd[0], buffer, 1024));
+		size = read(fd[0], buffer, 1024);
+		nbchar = write(fd[1], buffer, size);
+
+		if (nbchar == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+		if (size == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
+			exit(98);
+		}
 	}
 
 	for (; i < 2; i++)
