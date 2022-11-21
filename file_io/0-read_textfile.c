ef1ebf6edece9384ad3aeb51bd50de7101e57072
Gabriel <fossecruor@gmail.com>
2022-11-21 02:03:06 -0800

    adding if for err

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -6,10 +6,13 @@
  * Return: 0 if cannot read else return the number of char it could print
  */
 
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, cp;
+	int fd, cp, wr;
 	char *buffer;
+
+	if (filename == NULL)
+		return (0);
 
 	buffer = malloc(1 + sizeof(char) * letters);
 	if (buffer == NULL)
@@ -17,12 +20,17 @@ size_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+		return (0);
 
 	cp = read(fd, buffer, letters);
+	if (cp == -1)
+		return (0);
+
 	buffer[cp] = '\0';
 
-	write(1, buffer, cp);
+	wr = write(1, buffer, cp);
+	if (wr == -1)
+		return (0);
 
 	close(fd);
 	return (cp);
