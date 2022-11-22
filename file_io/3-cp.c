5e7dc3fd01e13c09c216b3957dd64dc9d846ecab
Gabriel <fossecruor@gmail.com>
2022-11-22 00:33:36 -0800

    move lignes and change perms

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int ac, char **av)
 		exit(98);
 	}
 
-	fd[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0600 | 0060 | 0004);
 	if (fd[1] == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
@@ -33,21 +33,18 @@ int main(int ac, char **av)
 	while (nbchar > 0)
 	{
 		size = read(fd[0], buffer, 1024);
+		if (size == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
+			exit(98);
+		}
 		nbchar = write(fd[1], buffer, size);
-
 		if (nbchar == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-		if (size == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
-			exit(98);
-
-		}
 	}
-
 	for (; i < 2; i++)
 	{
 		if (close(fd[i]) == -1)
