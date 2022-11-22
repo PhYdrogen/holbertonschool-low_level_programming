7546decd71f76eeaf5c1e003104ffff995131b55
Gabriel <fossecruor@gmail.com>
2022-11-22 04:40:00 -0800

    free something

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,9 +14,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
-		free(new);
+		free((*head)->prev);
 		exit(99);
 	}
+
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
