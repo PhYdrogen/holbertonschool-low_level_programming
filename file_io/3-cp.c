9fe57efd963b6e1292184f00189c5543a5e2dd04
Gabriel <fossecruor@gmail.com>
2022-11-22 00:49:46 -0800

    commented fd1 opening

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -25,11 +25,11 @@ int main(int ac, char **av)
 	}
 
 	fd[1] = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0600 | 0060 | 0004);
-	if (fd[1] == -1)
+	/*if (fd[1] == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[2]);
 		exit(98);
-	}
+	}*/
 	while (nbchar > 0)
 	{
 		size = read(fd[0], buffer, 1024);
