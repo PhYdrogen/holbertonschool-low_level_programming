fb7962b92548524c560e9485a7b7c3a974cba8df
Gabriel <fossecruor@gmail.com>
2022-11-22 05:49:16 -0800

    if head is null dont

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx,
 	{
 		new->prev = NULL;
 		new->next = *head;
-		(*head)->prev = new;
+		if (*head != NULL)
+			(*head)->prev = new;
 		*head = new;
 		return (new);
 	}
