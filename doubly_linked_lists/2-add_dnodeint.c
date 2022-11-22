2555ad910979834545d6c68e330298eae68259c7
Gabriel <fossecruor@gmail.com>
2022-11-22 02:19:00 -0800

    adding condition for prev pointer

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,6 +16,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		exit(99);
 
 	new->n = n;
+	if (new->prev == NULL)
+		new->prev = (*head)->next;
+	else
+		new->prev = NULL;
+
 	new->next =  *head;
 	*head = new;
 
