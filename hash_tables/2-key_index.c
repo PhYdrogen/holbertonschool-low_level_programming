6829d127fe3a753ca41b32249aa7a9e48ae15d50
Gabriel <fossecruor@gmail.com>
2022-11-23 08:27:28 -0800

    Betty coding DOC

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
-
+/**
+ * key_index - get the index for a key
+ * @key: the string to get the key
+ * @size: the size of the array
+ * Return: the index
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
