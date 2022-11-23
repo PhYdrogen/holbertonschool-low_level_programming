d999c1cd64dadf017cf1072dc83bc8a87c6a2dd3
Gabriel <fossecruor@gmail.com>
2022-11-23 07:54:02 -0800

    malloc instead of calloc

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,6 +13,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array =  calloc(size, sizeof(hash_node_t));
+	table->array =  malloc(sizeof(hash_node_t));
 	return (table);
 }
