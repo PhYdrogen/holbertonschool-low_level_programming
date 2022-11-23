a197841d2229b3652df3e54e353c94a2ecddac7c
Gabriel <fossecruor@gmail.com>
2022-11-23 07:48:26 -0800

    comment to know size

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -6,8 +6,6 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
-
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
@@ -15,8 +13,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
-	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+	/*  table->array =  calloc(table->size, sizeof(hash_node_t)); */
 	return (table);
 }
