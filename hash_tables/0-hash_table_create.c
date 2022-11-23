d74228161bb8c1520f96a7cbd530ef2b1cb22cac
Gabriel <fossecruor@gmail.com>
2022-11-23 02:49:29 -0800

    init git

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hashtable
+ * @size: the size of the table
+ * Return: a ptr to the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	return (table);
+}
