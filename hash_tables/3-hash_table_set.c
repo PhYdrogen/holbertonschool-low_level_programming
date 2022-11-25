bd73c6e56820b84591234d225167c4f5cf29bbb7
Gabriel <fossecruor@gmail.com>
2022-11-25 00:50:15 -0800

    update the hashtable.h and index convert the key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,10 +1,41 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
+ * hash_table_set - add element to hash table
+ * @ht: the hash table
+ * @key: the key cannot be empty
+ * @value: is the value is assciated with the key
+ * Return: 1 if succeed
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	int index;
+	hash_node_t *new, *tmp;
 
+	if ((ht == NULL) || (key == NULL) || (strlen(key) == 0))
+		return (0);
+
+	index = key_index((unsigned char *) key, ht->size);
+
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+		return (1);
+	}
+	else
+	{
+		if (ht->array[index]->key == key)
+		{
+			tmp = ht->array[index];
+			new->next = tmp;
+			ht->array[index] = new;
+			return (1);
+		}
+	}
+	return (1);
 }
