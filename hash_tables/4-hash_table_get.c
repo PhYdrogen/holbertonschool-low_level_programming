5593bc0f732ee8dfcec59467aed9133089d67e7d
Gabriel <fossecruor@gmail.com>
2022-11-25 02:04:41 -0800

    add content

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get the value for the key
+ * @ht: the hash table
+ * @key: the key
+ * Return: the value associated with the key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index;
+
+	index = 0;
+
+	index = key_index((unsigned char *)key, ht->size);
+	return (ht->array[index]->value);
+}
